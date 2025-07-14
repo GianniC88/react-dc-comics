16ff740053ed74ea519ce5dbef7aee8e673da1b0
GianniC88 <gianni.colucci.88@gmail.com>
2025-07-14 23:39:20 +0200

    Ristruttura il componente Header e aggiungi stili CSS per la navigazione

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,23 @@ export default function Header(){
 
 
 		 <header>
-			<img src = {headLogo}/>
-      
+			<div className = "containe d-flex justify-content-between ">
+			<img className ="p-3" src = {headLogo}/>
+      <nav class="navbar navbar-expand">
+		<div class="nav navbar-nav  me-5">
+			<a class="" href="#">Caracters</a>
+			<a class="active" href="#">Commics</a>
+			<a class="" href="#">Movies</a>
+			<a class="" href="#">TV</a>
+			<a class="" href="#">Games</a>
+			<a class="" href="#">Videos</a>
+			<a class="" href="#">Fans</a>
+			<a class="" href="#">News</a>
+			<a class="" href="#">Shop</a>
+		</div>
+	  </nav>
+		</div>
+	  
      
     </header>  
 	)
