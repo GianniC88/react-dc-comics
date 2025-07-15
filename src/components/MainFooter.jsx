50f4d86f06fd0e60da30500be3b66a22efbc7eca
GianniC88 <gianni.colucci.88@gmail.com>
2025-07-15 02:21:54 +0200

    Rimuovi il componente navIcon e integra il suo contenuto direttamente in MainFooter; migliora la formattazione del codice CSS

--- src/components/MainFooter.jsx
+++ src/components/MainFooter.jsx
@@ -1,9 +1,21 @@
-import navIcon from './navIcon'
+
 
 export default function MainFooter (){
 	return(
 		<>
-		<navIcon/>
+		<div className = "container  ">
+		
+     	   
+				<div className="nav navbar-nav d-flex justify-content-between ">
+					<a className="" href="#"><img src = "/img/buy-comics-digital-comics.png"></img>digital comics</a>
+					<a className="" href="#"><img src = "/img/buy-comics-merchandise.png"></img>DC Merchandise</a>
+					<a className="" href="#"><img src = "/img/buy-buy-comics-shop-locator.png"></img>Subsription</a>
+					<a className="" href="#"><img src = "/img/buy-comics-shop-locator.png"></img>Comic Shop Locator</a>
+					<a className="" href="#"><img src = "/img/buy-comics-subscriptions.png"></img>DC power visa</a>
+					
+				</div>
+	  	    
+		</div>
 		</>
 	)
 }
