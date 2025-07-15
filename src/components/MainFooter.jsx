90c2f9796668e891a518e5108d356b9b14c796a4
GianniC88 <gianni.colucci.88@gmail.com>
2025-07-15 12:53:23 +0200

    Ristruttura il componente MainFooter; sposta la definizione dell'array icon all'inizio e migliora la formattazione del codice

--- src/components/MainFooter.jsx
+++ src/components/MainFooter.jsx
@@ -1,12 +1,53 @@
+const icon=[
+		{
+			id:1,
+			href:'#',
+			src:'/img/buy-comics-digital-comics.png',
+			name:'DIGITAL COMICS',
+
+		},
+		{
+			id:2,
+			href:'#',
+			src:"/img/buy-comics-merchandise.png",
+			name: 'DC MERCHANDISE',
+
+		},
+		{
+			id:3,
+			href:'#',
+			src:"/img/buy-comics-subscriptions.png",
+			name: 'SUBSCRIPTION',
+
+		},
+		{
+			id:4,
+			href:'#',
+			src:"/img/buy-comics-shop-locator.png",
+			name: 'COMIC SHOP LOCATOR',
+
+		},
+		{
+			id:5,
+			href:'#',
+			src:'/img/buy-comics-digital-comics.png',
+			name: 'DC POWER VISA',
+
+		}
+
+	]
+
 export default function MainFooter (){
 
-	const icon 
+	
 	return(
 		<>
 			<div className="container bg-primary ">
 		
      	   
 				<div className="d-flex justify-content-between gap-5">
+
+
 					<a className="text-white row" href="#"><img src="/img/buy-comics-digital-comics.png"></img>digital comics</a>
 					<a className="text-white row" href="#"><img src="/img/buy-comics-merchandise.png"></img>DC Merchandise</a>
 					<a className="text-white row" href="#"><img src="/img/buy-comics-subscriptions.png"></img>Subsription</a>
