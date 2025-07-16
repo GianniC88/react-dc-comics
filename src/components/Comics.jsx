cfdcd2488eeb68b4cd7ebf643d615c803197d91b
GianniC88 <gianni.colucci.88@gmail.com>
2025-07-17 00:54:11 +0200

    Correggi il nome della funzione di esportazione in ArrayComics e aggiorna il rendering dei fumetti per utilizzare i dati importati da ArrayComics.

--- src/components/Comics.jsx
+++ src/components/Comics.jsx
@@ -1,14 +1,16 @@
 import PropsComics from "./props/PropsComics";
-import comics from "./ArrayComics";
+import ArrayComics from "./ArrayComics";
 
-export default function comics() {
+export default function ArrayComics() {
   return (
     <div className="jumbotron jumbotron-fluid bg-dark p-5 position-relative ">
       <div className="container">
         <div className="row ">
           <h4 className="currentSeries">CURRENT SERIE</h4>
 
-          {<PropsComics src="src" title="title" />}
+          {comics.map(({ title, src }) => {
+            return <PropsComics src={src} title={title} />;
+          })}
 
           <h4 className="loadMore">LOAD MORE</h4>
         </div>
