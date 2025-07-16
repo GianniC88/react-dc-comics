ae3be92166877badbdbf86c26f2a3e08d5eabb9d
GianniC88 <gianni.colucci.88@gmail.com>
2025-07-17 01:45:32 +0200

    Rinomina il componente ArrayComics in Comics e aggiorna il rendering per utilizzare correttamente i dati dei fumetti. Modifica PropsComics per accettare le proprietà corrette e rimuovi il console.log.

--- src/components/Comics.jsx
+++ src/components/Comics.jsx
@@ -1,16 +1,20 @@
 import PropsComics from "./props/PropsComics";
-import ArrayComics from "./ArrayComics";
+import { comics } from "./ArrayComics";
 
-export default function ArrayComics() {
+export default function Comics() {
   return (
     <div className="jumbotron jumbotron-fluid bg-dark p-5 position-relative ">
       <div className="container">
         <div className="row ">
           <h4 className="currentSeries">CURRENT SERIE</h4>
 
-          {comics.map(({ title, src }) => {
-            return <PropsComics src={src} title={title} />;
-          })}
+          {comics.map(({ id, thumb, title }) => (
+            <PropsComics
+              key={id}
+              src={thumb}
+              title={title.replace(/#1/g, "")}
+            />
+          ))}
 
           <h4 className="loadMore">LOAD MORE</h4>
         </div>
--- src/components/props/PropsComics.jsx
+++ src/components/props/PropsComics.jsx
@@ -1,9 +1,8 @@
-export default function PropsComics(props) {
-  console.log(props);
+export default function PropsComics({ id, src, title }) {
   return (
-    <div key={id} className="col-2 mb-5">
+    <div className="col-2 mb-5">
       <div className="card bg-transparent align-items-center ">
-        <img className="card-img-top" src={thumb} alt="" />
+        <img className="card-img-top" src={src} alt="" />
         <div className="card-body">
           <h4 className="card-title text-white">{title}</h4>
           {/* <p>Description{description}</p> */}
@@ -26,14 +25,14 @@ export default function PropsComics(props) {
     <div className="card-body">
       <h4 className="card-title text-white">{title}</h4>
       {/* <p>Description{description}</p> */ //}
-{
-  /* <p>Price:{`$${price}`}</p>
+//{
+/* <p>Price:{`$${price}`}</p>
                       <p>Series: {series}</p>
                       <p>Sale Date: {sale_date}</p>
                       <p>Type: {type}</p>
                       <p>Artists: {artists.join(", ")}</p>
                       <p>Writers: {writers.join(", ")}</p> */
-}
+//}
 // </div>
 // </div>
 //</div>;
